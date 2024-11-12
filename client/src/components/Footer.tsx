ec009cee413497c341b9da0efe6c74dbfee5ef87
DamienD44 <damas44@gmail.com>
2024-11-12 16:35:41 +0100

    ✅ ticket US08 check

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,101 +1,57 @@
 import { Link } from "react-router-dom";
+import "./Footer.css";
 
 const Footer: React.FC = () => {
   return (
-    <footer
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        padding: "20px",
-        background: "#333",
-        color: "#fff",
-      }}
-    >
+    <footer className="footer">
       {/* Section Langues */}
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <img
-          src="Icones/choix de langue.png"
-          alt="Changer de langue"
-          style={{
-            width: "40px",
-            cursor: "pointer",
-            marginRight: "10px",
-          }}
-        />
+      <div>
+        <img src="Icones/choix de langue.png" alt="Changer de langue" />
       </div>
 
       {/* Section Informations et Liens */}
-      <div style={{ textAlign: "center", flex: 1 }}>
+      <div className="Mentions">
         <p>© 2024 Tous droits réservés, SpotyWild®</p>
         <p>
-          <Link
-            to="/privacy-policy"
-            style={{
-              color: "#fff",
-
-              marginRight: "10px",
-            }}
-          >
+          <Link className="Politique" to="/Politique de confidentialité">
             Politique de confidentialité
           </Link>
-          |
-          <Link
-            to="/terms"
-            style={{
-              color: "#fff",
-
-              marginLeft: "10px",
-            }}
-          >
+          <span>|</span>
+          <Link className="Condition" to="/Conditions générales">
             Conditions générales
           </Link>
         </p>
       </div>
 
       {/* Section Réseaux Sociaux */}
-      <div style={{ display: "flex", alignItems: "center" }}>
+      <div className="icone">
         <a href="https://x.com" target="_blank" rel="noopener noreferrer">
-          <img
-            src="Icones/icone twitter.png"
-            alt="twitter"
-            style={{ width: "30px", marginRight: "5px" }}
-          />
+          <img src="Icones/txitter.png" alt="twitter" />
         </a>
         <a
           href="https://facebook.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
-            src="Icones/facebook.png"
-            alt="Facebook"
-            style={{ width: "30px", marginRight: "5px" }}
-          />
+          <img src="Icones/Facebook.png" alt="Facebook" />
         </a>
-        <a href="https://spotify.com" target="_blank" rel="noopener noreferrer">
-          <img
-            src="Icones/icone spotify.jpg"
-            alt="Spotify"
-            style={{ width: "30px", marginRight: "5px" }}
-          />
+        <a
+          className="spoty"
+          href="https://spotify.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="Icones/spotify.png" alt="Spotify" />
         </a>
         <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-          <img
-            src="Icones/youtube.png"
-            alt="YouTube"
-            style={{ width: "30px", marginRight: "5px" }}
-          />
+          <img src="Icones/Youtube.png" alt="YouTube" />
         </a>
         <a
           href="https://instagram.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
-            src="Icones/Instagram.png"
-            alt="Instagram"
-            style={{ width: "30px", marginRight: "5px" }}
-          />
+          <img src="Icones/Instagram.png" alt="Instagram" />
         </a>
       </div>
     </footer>
